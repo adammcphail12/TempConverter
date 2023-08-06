a8c95c379d1bec75e76b4a54d46dab4596faaf44
adammcphail12 <99302328+adammcphail12@users.noreply.github.com>
2023-08-07 11:02:49 +1200

    Add files via upload

---
+++ history_gui_v1.py
@@ -0,0 +1,80 @@
+from tkinter import *
+from functools import partial # to prevent unwanted windows
+
+class Converter:
+    
+    def __init__(self):
+
+        #common format for all buttons
+        BUTTON_FONT = ('Arial', '12', 'bold')
+        BUTTON_FG = '#ffffff'
+
+
+        #set up Gui Frame
+        self.temp_frame = Frame(padx=10,pady=10)
+        self.temp_frame.grid()
+
+
+        #button frame 
+        self.button_frame = Frame(self.temp_frame)
+        self.button_frame.grid(row=0)
+
+
+        # history info button
+        self.to_history_button = Button(self.button_frame, text = 'History / Export', bg = '#004C99', fg = BUTTON_FG ,font= BUTTON_FONT, width = 12, command=self.to_history)
+        self.to_history_button.grid(row = 1, column = 1, padx = 5, pady=5)
+
+        # renove when intergrating 
+        self.to_history_button.config(state = NORMAL)
+    def to_history(self):
+        HistoryExport(self)
+
+
+class HistoryExport:
+    def __init__(self, partner):
+        # setup dialouge box and back ground color 
+        self.history_box = Toplevel()
+
+        #  disable the history / export button
+        partner.to_history_button.config(state = DISABLED)
+        # if the user press cross at the top , closes help box and release help button
+        self.history_box.protocol('WM_DELETE_WINDOW', partial(self.close_history, partner))
+
+        # set up new frame and grid
+        self.history_frame = Frame(self.history_box, width = 300, height = 200)
+        self.history_frame.grid()
+
+        # heading label
+        self.history_heading_label = Label(self.history_frame, text = 'History / Export', font = ('Arial', '14', 'bold'), wrap = 350, justify = 'left')
+        self.history_heading_label.grid(row = 0)
+
+        # text 1 label
+        history_text_1 = 'Below are your recent calculations - showing 3 / 3 calculations. All calculations are shown to the nearest degree'
+        self.history_text_1_label = Label(self.history_frame, text = history_text_1, wrap = 350, justify = 'left')
+        self.history_text_1_label.grid(row = 1, padx = 10)
+
+        # calculations label
+        calculations = ['example\n', 'example\n', 'example\n']
+        self.history_calculations_label = Label(self.history_frame, text= calculations, justify = 'center', bg = '#ffffff', width = 46)
+        self.history_calculations_label.grid(row = 2, padx = 10, pady = 10)
+
+
+        # dismiss button
+        self.dismiss_button = Button(self.history_frame, font = ('Arial', '12', 'bold'), text = 'Dismiss', bg = '#666666', fg = '#ffffff', command = partial(self.close_history, partner))
+        self.dismiss_button.grid(row = 3, padx = 10, pady = 10)
+
+        #close help dialouge (used by x at top of dialouge)
+    def close_history(self, partner):
+        # put help back to normal ...
+        partner.to_history_button.config(state = NORMAL)
+        self.history_box.destroy()
+
+
+# main routine
+if __name__ == '__main__':
+    root = Tk()
+    root.title('Temputure Converter')
+    #calling Converter Class
+    Converter()
+    root.mainloop()
+
